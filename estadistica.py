b54ef761551fbc48f6432debf0fd99fd581b0126
davidgfb <davidgracia99@gmail.com>
2020-04-20 17:13:52 +0200

    Add files via upload

--- estadistica.py
+++ estadistica.py
@@ -3,42 +3,110 @@ casosTotales=54 #26b+17r+10g+1y
 
 
 
+
+
+
+recuentoGlobal=[]
+recuentoVacio=[]
 recuento=[]
-def recuento(mensaje):
+def guarda(mensaje):
     '''
     PRE: mensaje=['b','r','g','y']
     '''
-    if "b"==mensaje=="r" or "g"==mensaje=="y":
-        recuento.append(mensaje)
-    else:
-        raise ValueError()
+    recuento.append(mensaje)
+    print('Valor guardado')
 
-'''        
+'''
 #PROBADOR
+print(recuento,'debe ser []')
+guarda('a')
+print(recuento,"debe ser ['a']")
+'''
+
+
+
+def resetea():
+    global recuento
+    recuento=recuentoVacio
+    print('Recuento reseteado')
+
+'''
+#PROBADOR
+guarda('a')
+print(recuento,"debe ser ['a']")
+resetea()
+print(recuento,'debe ser []')
+'''
+
+
+def cuentaCaracteres(mensaje):
+    caracteres=0
+    for caracter in recuento:
+        if caracter==mensaje:
+            caracteres+=1
+    return caracteres
+
+'''
+#PROBADOR
+guarda('b')
+print(cuentaCaracteres('b'),'debe ser 1')
+'''
+
+def porcentaje(mensaje):
+    return 100*mensaje/54
+
+'''
+#PROBADOR
+print(porcentaje(1),'debe ser 1.8518518518518519')
+'''
+
+
+def muestraRecuento():
+    print('recuento: ',recuento,' b:',porcentaje(cuentaCaracteres('b')),'%',
+          'r:',porcentaje(cuentaCaracteres('r')),'%',
+          'g:',porcentaje(cuentaCaracteres('g')),'%',
+          'y:',porcentaje(cuentaCaracteres('y')),'%')
+
+'''
+#PROBADOR
+guarda('b')
+muestraRecuento()
+print("debe ser recuento:  ['b']  b: 1.8518518518518519 % r: 0.0 % g: 0.0 % y: 0.0 %")
+'''
+
+
+
 abierto=True
 while abierto:
-    try:
-        mensaje=input('Introduzca caractér o comando: ')
-        
-        if mensaje=='sal' or mensaje=="cierra":
-        #if 'sal'==mensaje=='cierra': no funciona
-            abierto=False
-        elif mensaje=='recuento':
-            print(recuento)
-        else:
-            
-                recuento(mensaje)
-    except:
-        mensaje1=input('Caractér o comando no reconocido. Vuelva a introducirlo: ')
-        recuento(mensaje1)
-'''    
+    mensaje=input('Introduzca caractér o comando: ')
     
+    if mensaje=='sal' or mensaje=="cierra": #(1) if 'sal'==mensaje=='cierra':
+        #(1) no funciona
+        abierto=False
+    elif mensaje=='recuento':
+        muestraRecuento()
+    elif mensaje=='resetea':
+        resetea()
+    elif mensaje=="b" or mensaje=="r" or mensaje=="g" or mensaje=="y":
+        #(1*) no funciona
+        guarda(mensaje)
+    else:
+        print('Caractér o comando no reconocido.')
+     
+  
+
+
+
+
+
+
+
 
 
 
 def esNegativo(n):
     '''
-    PRE: 0<n<0 
+    PRE: 0>n>0 
     '''
     negativo=False
     if n<0 or n==-0:
@@ -82,7 +150,7 @@ def factoriza(n):
             factores.append(i)
     return factores
 
-
+'''
 #PROBADOR
 print(factoriza(13),"debe ser [1,13]")
 print(factoriza(26),"debe ser [1,2,13,26]")
@@ -91,7 +159,7 @@ print(factoriza(10),"debe ser [1,2,5,10]")
 print(factoriza(1),"debe ser [1]")
 
 print(factoriza(-1),"debe ser [-1] + exc")
-
+'''
 
 def esPrimo(n):
     '''
