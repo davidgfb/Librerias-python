a7bf75db327337a52198fe4c1ed7533c54b9307d
davidgfb <davidgracia99@gmail.com>
2020-12-05 20:19:54 +0100

    Update barraProgreso.py

--- barraProgreso.py
+++ barraProgreso.py
@@ -1,3 +1,3 @@
 from time import sleep
-'''abre desde cmd'''
+'''"\r" es carriage return abre desde cmd'''
 [(print(i,"% [",i*"#",end="]\r"),sleep(1)) for i in range(101)] #list comprehensions, faltan formatting strings "%d" "%s"
